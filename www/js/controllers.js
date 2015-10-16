286f1880526882b1441a5609d47b521d542cc30d
Faisal Shaik <fshaik@LCLaptop78.local>
2015-10-16 15:24:26 -0700

    changed icons and hieght

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,5 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-
-
-})
-
 .controller('MallMapCtrl', function($scope){
 
   google.maps.event.addDomListener(window, "load", function() {
@@ -22,25 +17,6 @@ angular.module('starter.controllers', [])
 
   });
 
-})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('SplashCtrl', function($scope, $state, User){
